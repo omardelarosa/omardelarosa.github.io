11d630747b8b527365d23ecfe0e25f463d315afe
Omar Delarosa <thedelarosa@gmail.com>
2017-11-28 11:22:09 -0500

    updating deploy script to include cache purging, removing zeit

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# deploy to zeit
-now --static
+# push to github
+echo "Pushing to Github..."
+git push origin master
 
-# create alias
-now alias
+# purge cloudflare cache
+echo "Purging CloudFlare cache..."
+curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache" \
+     -H "X-Auth-Email: $CLOUDFLARE_AUTH_EMAIL" \
+     -H "X-Auth-Key: $CLOUDFLARE_AUTH_API_KEY" \
+     -H "Content-Type: application/json" \
+     --data '{"purge_everything":true}'
+echo "\n"
 
+echo "Uploading site maps to Google"
 # notify google of sitemap change
 curl 'http://www.google.com/webmasters/sitemaps/ping?sitemap=https://www.omardelarosa.com/dist/sitemap.xml'
